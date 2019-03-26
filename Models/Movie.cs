7ce5c2c5983d3876d114f116df96e761a72be62b
Wrekkler <t.grzesik19@gmail.com>
2019-03-26 14:54:46 +0100

    Added search to page

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,9 +9,15 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        //Display attribute specifies what to display for the name of a field
+        //in this case "Release Date" instead of "ReleaseDate"
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        
+        //data annotation enables EF Core to correctly map Price to currency in DB
+        [Column(TypeName = "decimal(18, 2)")]
         public Decimal Price { get; set; }
     }
 }
--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPagesMovie.Pages.Movies
 {
@@ -17,12 +18,49 @@ namespace RazorPagesMovie.Pages.Movies
         {
             _context = context;
         }
-
+    
         public IList<Movie> Movie { get;set; }
+        
+        //BindProperty binds form values and query strings with the same name as the property
+        [BindProperty(SupportsGet = true)]
+        
+        //SearchString contains the text user enter in search text box
+        public string SearchString {get; set;}
+        
+        //Genres contains the list of genres
+        public SelectList Genres {get;set;}
 
+        [BindProperty(SupportsGet = true)]
+
+        //MovieGenre contains the specific genre the user selects (for example "Comedy")
+        public string MovieGenre {get; set;}
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            //Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            //Creating LINQ query to select the movies
+            var movies = from m in _context.Movie
+                         select m;
+            //
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                //Contains maps to "SQL LIKE", in SQLite it's case sensitive
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                //retrives all the genres from database
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            //SelectList of genres is created by projecting the distinc genres
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());  
+            Movie = await movies.ToListAsync();
         }
     }
 }
